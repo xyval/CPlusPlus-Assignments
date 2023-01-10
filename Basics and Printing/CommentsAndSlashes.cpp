89eb22c03e79671b476cbebe29320c9d795a51e6
xyval <111503969+xyval@users.noreply.github.com>
2023-01-10 14:58:54 +0800

    Create CommentsAndSlashes.cpp

---
+++ Basics and Printing/CommentsAndSlashes.cpp
@@ -0,0 +1,12 @@
+//Basic and Printing Comment and Slashes Valerie 3 points 
+#include <iostream>
+
+int main() {
+  // A comment, this is so you can read your program later.
+  // Anything after the // is ignored by C++.
+  std::cout << "I could have code like this."; 
+  // and the comment after is ignored.
+  // You can also use a comment to "disable" or comment out a piece of code:
+  // System.out.println("This won't run.");
+  std::cout << "This will run.";
+}
