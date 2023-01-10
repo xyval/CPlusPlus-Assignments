7142dfe1e99c51fb4d1370fccbf523bce4d41129
xyval <111503969+xyval@users.noreply.github.com>
2023-01-10 14:51:37 +0800

    Create GoodFirstProgram.cpp

---
+++ Basics and Printing/GoodFirstProgram.cpp
@@ -0,0 +1,6 @@
+//Basics and Printing	A Good First Program Valerie 4 points
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!\nHello Again\nI like typing this.\nThis is fun.\nYay! Printing.\nI'd much rather you 'not'.\nI \"said\" do not touch this.\n";
+}
