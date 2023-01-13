0d40a8ae37a15aa8ef206f45b63260a027643c18
xyval <111503969+xyval@users.noreply.github.com>
2023-01-13 14:58:28 +0800

    Create UsingVariables.cpp

---
+++ Variables/UsingVariables.cpp
@@ -0,0 +1,16 @@
+//Variables	Using Variables	9 points Valerie 
+#include <iostream>
+using namespace std;
+int main() {
+    string subject;
+    int room;
+    double e; 
+    subject = "Computer Science";
+    room = 113;
+    e = 2.71828;
+    
+    cout << "This is room # "<< room <<"\n";
+    cout << "e is close to "<< e<<"\n";
+    cout << "I am learning a bit about "<< subject<<"\n";
+  return 0;
+	}
