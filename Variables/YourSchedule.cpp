9630b47330814e9b7dd984e2e6fb84165d2ff616
xyval <111503969+xyval@users.noreply.github.com>
2023-01-13 16:30:24 +0800

    Create YourSchedule.cpp

---
+++ Variables/YourSchedule.cpp
@@ -0,0 +1,32 @@
+//Variables	Your Schedule	15 points Valerie 
+#include <iostream>
+using namespace std;
+int main() {
+    string course1, course2, course3, course4, course5, course6, course7, course8;
+    string teacher1, teacher2, teacher3, teacher4, teacher5, teacher6, teacher7, teacher8; 
+		course1 = "English";
+		course2 = "Maths";
+		course3 = "Java";
+		course4 = "history";
+		course5 = "study hall";
+		course6 = "course 6";
+		course7 = "course 7";
+		course8 = "course 8";
+		teacher1 = "English Teacher";
+		teacher2 = "Math Teacher";
+		teacher3 = "Ms. Tina";
+		teacher4 = "History Teacher";
+		teacher5 = "Teacher";
+		teacher6 = "Teacher 6";
+		teacher7 = "Teacher 7";
+		teacher8 = "Teacher 8";
+    cout << "1. " << course1 << "     "<< teacher1 << "\n";
+		cout << "2. " << course2 << "       "<< teacher2 << "\n";
+		cout << "3. " << course3 << "        "<< teacher3 << "\n";
+		cout << "4. " << course4 << "     "<< teacher4 << "\n";
+		cout << "5. " << course5 << "  "<< teacher5 << "\n";
+		cout << "6. " << course6 << "    "<< teacher6 << "\n";
+		cout << "7. " << course7 << "    "<< teacher7 << "\n";
+		cout << "8. " << course8 << "    "<< teacher8 << "\n";
+  
+}
