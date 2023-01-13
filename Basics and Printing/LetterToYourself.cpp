866c6ab8067ba04cdbcdcc3a06e492dd6b1c09aa
xyval <111503969+xyval@users.noreply.github.com>
2023-01-13 13:39:44 +0800

    Create LetterToYourself.cpp

---
+++ Basics and Printing/LetterToYourself.cpp
@@ -0,0 +1,15 @@
+//Basics and Printing A Letter to Yourself 7 points Valerie
+
+#include <iostream>
+
+int main() {
+  std::cout << "+---------------------------------------------+\n"; 
+  std::cout << "|                                         ### |\n";
+  std::cout << "|                                         ### |\n";
+  std::cout << "|              Valerie Lin                    |\n";
+  std::cout << "|              1, Street Road                 |\n";
+  std::cout << "|              Town City, Country             |\n";
+  std::cout << "|                                             |\n";
+  std::cout << "+---------------------------------------------+\n";
+  return 0;
+}
