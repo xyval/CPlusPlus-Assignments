58ba786fc563860569dc1e1cc3005d32890f6a42
xyval <111503969+xyval@users.noreply.github.com>
2023-01-13 13:52:20 +0800

    Create initial.cpp

---
+++ Basics and Printing/initial.cpp
@@ -0,0 +1,10 @@
+//Basics and Printing Your Initials 6 points Valerie
+#include <iostream>
+
+int main() {
+  std::cout << "V     V  L\n"; 
+  std::cout << " V   V   L\n";
+  std::cout << "  V V    L\n";
+  std::cout << "   V     LLLLLLL\n";
+  return 0;
+}
