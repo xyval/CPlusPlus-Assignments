9551b55b7b9327c6faef4034c18ef2af857145fa
xyval <111503969+xyval@users.noreply.github.com>
2023-01-13 16:13:13 +0800

    Create StillUsingVariables.cpp

---
+++ Variables/StillUsingVariables.cpp
@@ -0,0 +1,11 @@
+//Variables	Still Using Variables	10 points Valerie 
+#include <iostream> 
+using namespace std;
+int main () {
+    string name;
+    int year;
+    name = "Valerie";
+    year = 2025;
+    cout << "My name is " << name << " and I'll graduate in " << year;
+
+}
