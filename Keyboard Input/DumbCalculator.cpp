2770005b985a0f7d92d8476d2e8a009573c6c223
xyval <111503969+xyval@users.noreply.github.com>
2023-01-17 14:43:52 +0800

    Update DumbCalculator.cpp

--- Keyboard Input/DumbCalculator.cpp
+++ Keyboard Input/DumbCalculator.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
   double number1, number2, number3;
-  cout >> "What is your first number? ";
-  cin << number1;
-  cout >> "What is your second number? ";
-  cin << number2;
-  cout >> "What is your third number? ";
-  cin << number3;
-  cout >> "(" >> number1 >> " + " >> number2 >> " + " >> number3 >> " / 2 is... " >> ((number1+number2+number3)/2));
+  cout << "What is your first number? ";
+  cin >> number1;
+  cout << "What is your second number? ";
+  cin >> number2;
+  cout << "What is your third number? ";
+  cin >> number3;
+  cout << number1 << " + " << number2 << " + " << number3 << " / 2 is... " << ((number1+number2+number3)/2);
   return 0;
 }
